86d01e4122b103a10b3b3569b7a31ee88679c569
Faisal Al Qasimi <falqas@gmail.com>
2024-04-13 11:04:15 -0400

    Add tests & features exc: show all by & unplayed by

--- index.js
+++ index.js
@@ -1,28 +1,139 @@
 const readline = require("readline");
 
-// Initially thought I'd use a Map, but then decided to use an array as it more closely mimics the "show all" behavior (first in first out) specified in the prompt
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+/* Initially thought I'd use a Map for O(1) album lookup, but then decided to use an array
+   as it more closely mimics the "show all" behavior (first in first out) in the prompt
+*/
 const musicCollection = [];
+
+init();
 
 function init() {
     console.clear();
     console.log("Welcome to your music collection!");
 }
 
-function addAlbum(title, artist) {}
+function isDuplicateTitle(title) {
+    return musicCollection.some((album) => {
+        return album.title.toLowerCase() === title.toLowerCase();
+    });
+}
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+function addAlbum(artist, title) {
+    if (!isDuplicateTitle(title)) {
+        musicCollection.push({
+            artist,
+            title,
+            isPlayed: false,
+        });
+        console.log(`Added "${title}" by "${artist}"`);
+    } else {
+        console.log(`Title "${title}" already exists`);
+    }
+}
+function playAlbum(title) {
+    console.log("musicCollection", musicCollection);
+    console.log("title", title);
+
+    const albumIndex = musicCollection.findIndex(
+        (album) => album.title.toLowerCase() === title.toLowerCase()
+    );
+
+    if (albumIndex !== -1) {
+        musicCollection[albumIndex].isPlayed = true;
+        console.log(`You're listening to "${title}"`);
+    } else {
+        console.log(`"${title}" not found`);
+    }
+}
+
+function showAll(optionalArtist) {
+    if (optionalArtist) {
+        const albums = musicCollection.filter(
+            (album) =>
+                album.artist.toLowerCase() === optionalArtist.toLowerCase()
+        );
+        albums.forEach((album) => {
+            console.log(
+                `${album.title} by ${album.artist} (${
+                    album.isPlayed ? "played" : "unplayed"
+                })`
+            );
+        });
+    } else {
+        musicCollection.forEach((album) => {
+            console.log(
+                `${album.title} by ${album.artist} (${
+                    album.isPlayed ? "played" : "unplayed"
+                })`
+            );
+        });
+    }
+}
+
+function showUnplayed(optionalArtist) {
+    if (optionalArtist) {
+        const albums = musicCollection.filter(
+            (album) =>
+                album.artist.toLowerCase() === optionalArtist.toLowerCase() &&
+                album.isPlayed === false
+        );
+        albums.forEach((album) => {
+            console.log(`${album.title} by ${album.artist}`);
+        });
+    } else {
+        const unplayed = musicCollection.filter(
+            (album) => album.isPlayed === false
+        );
+        unplayed.forEach((album) => {
+            console.log(`${album.title} by ${album.artist}`);
+        });
+    }
+}
 
 function getUserInput(rl) {
     rl.question("> ", (input) => {
+        // TODO trim input
         if (input === "quit") {
             console.log("Bye!");
             rl.close();
-        } else {
-            getUserInput();
+        } else if (input.startsWith("add")) {
+            const parsedInput = input
+                .slice(3) // Ignore the "add " portion
+                .split('"')
+                .filter((part) => part.trim() !== "")
+                .map((part) => part);
+            const [title, artist] = parsedInput;
+            addAlbum(artist, title);
+        } else if (input.startsWith("play")) {
+            const parsedInput = input
+                .slice(4) // Ignore the "play " portion
+                .split('"')
+                .filter((part) => part.trim() !== "")
+                .map((part) => part);
+            const title = parsedInput[0];
+            playAlbum(title);
+        } else if (input.startsWith("show")) {
+            const parts = input.split(" ");
+            const showType = parts[1];
+            const artist = parts.slice(2).join(" ").trim().replace(/"/g, "");
+            // TODO show all by
+            // TODO show unplayed by
+            if (showType === "all" && artist) {
+                showAll(artist);
+            } else if (showType === "unplayed" && artist) {
+                showUnplayed(artist);
+            } else if (showType === "all") {
+                showAll();
+            } else if (showType === "unplayed") {
+                showUnplayed();
+            }
         }
+        getUserInput(rl);
     });
 }
 if (process.env.NODE_ENV !== "test") {
@@ -31,4 +142,9 @@ if (process.env.NODE_ENV !== "test") {
 
 module.exports = {
     getUserInput,
+    addAlbum,
+    playAlbum,
+    musicCollection,
+    showAll,
+    showUnplayed,
 };
--- index.test.js
+++ index.test.js
@@ -1,6 +1,13 @@
 jest.mock("readline");
 const readline = require("readline");
-const { getUserInput } = require("./index.js");
+const {
+    getUserInput,
+    addAlbum,
+    musicCollection,
+    playAlbum,
+    showAll,
+    showUnplayed,
+} = require("./index.js");
 
 describe("getUserInput", () => {
     const mockQuestion = jest.fn();
@@ -13,9 +20,11 @@ describe("getUserInput", () => {
         close: mockClose,
     });
 
-    beforeEach(() => {});
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
 
-    it('should call console.log with "Bye!" and close the prompt on user inputting "quit"', () => {
+    it('should close the prompt on user inputting quit and log "Bye!"', () => {
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout,
@@ -25,27 +34,99 @@ describe("getUserInput", () => {
         expect(console.log).toHaveBeenCalledWith("Bye!");
         expect(mockClose).toHaveBeenCalled();
     });
+    //   TODO it should play ie getUserInput('play Abbey Road');
 });
 
 describe("music collection", () => {
-    test("init", () => {
-        // text input for users, empty library, "welcome to your music collection"
+    beforeEach(() => {
+        musicCollection.length = 0;
+    });
+    it("should be empty at start", () => {
+        expect(musicCollection).toEqual([]);
+    });
+    it("adds title and artist to library when added and sets isPlayed to false", () => {
+        addAlbum("The Beatles", "Abbey Road");
+        expect(musicCollection).toEqual([
+            {
+                artist: "The Beatles",
+                title: "Abbey Road",
+                isPlayed: false,
+            },
+        ]);
     });
 
-    test('quits on user inputting "quit"', () => {});
-
-    test("adds title and artist to library when added and sets isPlayed to false", () => {});
-
-    test("plays album on user inputting play $album and sets isPlayed to true", () => {
-        // "youre listening to $album"
+    it("should prevent user from adding album with same name to collection (even if different artist)", () => {
+        addAlbum("The Beatles", "Abbey Road");
+        addAlbum("Fleetwood Mac", "Abbey road");
+        expect(musicCollection).toEqual([
+            {
+                artist: "The Beatles",
+                title: "Abbey Road",
+                isPlayed: false,
+            },
+        ]);
     });
-    test('shows all with isPlayed status on user inputting "show all"', () => {});
-    test('shows unplayed on user inputting "show unplayed"', () => {});
-    test('shows all by artist on user input "show all by $artist"', () => {});
-    test('shows all unplayed by artist on user input "show unplayed by $artist"', () => {});
-    test("prevents user from adding album with same name to collection", () => {});
-    test('maybe: should be case insensitive when checking for duplicates and on user inputting "quit"', () => {});
+    it('should play album on user inputting "play $album" and set "isPlayed" to true', () => {
+        addAlbum("The Beatles", "Abbey Road");
+        playAlbum("Abbey Road");
+        expect(musicCollection).toEqual([
+            {
+                artist: "The Beatles",
+                title: "Abbey Road",
+                isPlayed: true,
+            },
+        ]);
+    });
+    it('should show all with "isPlayed" status on user inputting "show all"', () => {
+        addAlbum("The Beatles", "Abbey Road");
+        addAlbum("Fleetwood Mac", "Rumours");
+        addAlbum("Lauryn Hill", "The Miseducation of Lauryn Hill");
+        playAlbum("Abbey Road");
+        showAll();
+        expect(console.log).toHaveBeenCalledWith("The Beatles - Abbey Road");
+        expect(console.log).toHaveBeenCalledWith("Fleetwood Mac - Rumours");
+        expect(console.log).toHaveBeenCalledWith(
+            "Lauryn Hill - The Miseducation of Lauryn Hill"
+        );
+    });
 
-    test("maybe: should escape quotes/sanitize text", () => {});
-    // maybe also: colocate all user input tests in separate describe block?
+    it('should show unplayed on user inputting "show unplayed"', () => {
+        addAlbum("The Beatles", "Abbey Road");
+        addAlbum("Fleetwood Mac", "Rumours");
+        addAlbum("Lauryn Hill", "The Miseducation of Lauryn Hill");
+        playAlbum("Abbey Road");
+        showUnplayed();
+        expect(console.log).toHaveBeenCalledWith("Fleetwood Mac - Rumours");
+        expect(console.log).toHaveBeenCalledWith(
+            "Lauren Hill - The Miseducation of Lauryn Hill"
+        );
+    });
+
+    it('should show all by artist on user input "show all by $artist"', () => {
+        addAlbum("The Beatles", "Abbey Road");
+        addAlbum("The Beatles", "Rubber Soul");
+        addAlbum("Fleetwood Mac", "Rumours");
+        showAll("The Beatles");
+        expect(console.log).toHaveBeenCalledWith("The Beatles - Abbey Road");
+        expect(console.log).toHaveBeenCalledWith("The Beatles - Rubber Soul");
+    });
+
+    it('should show all unplayed by artist on user input "show unplayed by $artist"', () => {
+        addAlbum("The Beatles", "Abbey Road");
+        addAlbum("The Beatles", "Rubber Soul");
+        addAlbum("Fleetwood Mac", "Rumours");
+        playAlbum("Abbey Road");
+        showUnplayed("The Beatles");
+        expect(console.log).toHaveBeenCalledWith("The Beatles - Rubber Soul");
+    });
+    // TODO maybe it('should maybe: should be case insensitive when checking for duplicates and on user inputting "quit"', () => {});
+
+    // TODO maybe it('maybe: should escape quotes/sanitize text', () => {});
+    // TODO maybe also: colocate all user input tests in separate describe block?
+    // it('maybe: init', () => {
+    // TODO maybe    // text input for users, empty library, 'welcome to your music collection'
+    // });
+
+    // getUserInput('show all');
+    // 'youre listening to $album'
 });
