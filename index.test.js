41550c4c02ce6db5a1874946db857911ec04364e
Faisal Al Qasimi <falqas@gmail.com>
2024-04-14 15:21:42 -0400

    Update tests

--- index.test.js
+++ index.test.js
@@ -1,132 +1,122 @@
-jest.mock("readline");
-const readline = require("readline");
 const {
-    getUserInput,
-    addAlbum,
-    musicCollection,
-    playAlbum,
-    showAll,
-    showUnplayed,
-} = require("./index.js");
+  handleAddAlbum,
+  handlePlayAlbum,
+  handleShowAlbums,
+  handleShowUnplayed,
+  musicCollection,
+} = require('./index.js');
 
-describe("getUserInput", () => {
-    const mockQuestion = jest.fn();
-    const mockClose = jest.fn();
+const testCollection = [
+  { artist: 'The Beatles', title: 'Abbey Road', isPlayed: true },
+  { artist: 'The Beatles', title: 'Rubber Soul', isPlayed: false },
+  { artist: 'Fleetwood Mac', title: 'Rumours', isPlayed: false },
+  {
+    artist: 'Lauryn Hill',
+    title: 'The Miseducation of Lauryn Hill',
+    isPlayed: false,
+  },
+];
+
+describe('Music Collection Tests', () => {
+  beforeEach(() => {
+    musicCollection.length = 0; // clear musicCollection
     console.log = jest.fn();
-    readline.createInterface.mockReturnValue({
-        question: mockQuestion.mockImplementation((prompt, callback) =>
-            callback("quit")
-        ),
-        close: mockClose,
-    });
+  });
 
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  it('should be empty at start', () => {
+    expect(musicCollection).toEqual([]);
+  });
 
-    it('should close the prompt on user inputting quit and log "Bye!"', () => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-        getUserInput(rl); // Trigger the function
-        expect(mockQuestion).toHaveBeenCalledWith("> ", expect.any(Function));
-        expect(console.log).toHaveBeenCalledWith("Bye!");
-        expect(mockClose).toHaveBeenCalled();
-    });
-    //   TODO it should play ie getUserInput('play Abbey Road');
-});
+  it('adds title and artist to library when added and sets isPlayed to false', () => {
+    handleAddAlbum('The Beatles', 'Abbey Road');
+    expect(musicCollection).toEqual([
+      {
+        artist: 'The Beatles',
+        title: 'Abbey Road',
+        isPlayed: false,
+      },
+    ]);
+  });
 
-describe("music collection", () => {
-    beforeEach(() => {
-        musicCollection.length = 0;
-    });
-    it("should be empty at start", () => {
-        expect(musicCollection).toEqual([]);
-    });
-    it("adds title and artist to library when added and sets isPlayed to false", () => {
-        addAlbum("The Beatles", "Abbey Road");
-        expect(musicCollection).toEqual([
-            {
-                artist: "The Beatles",
-                title: "Abbey Road",
-                isPlayed: false,
-            },
-        ]);
-    });
+  it('should prevent user from adding album with same name to collection (even if different artist)', () => {
+    handleAddAlbum('The Beatles', 'Abbey Road');
+    handleAddAlbum('Fleetwood Mac', 'Abbey road');
+    expect(musicCollection).toEqual([
+      {
+        artist: 'The Beatles',
+        title: 'Abbey Road',
+        isPlayed: false,
+      },
+    ]);
+  });
 
-    it("should prevent user from adding album with same name to collection (even if different artist)", () => {
-        addAlbum("The Beatles", "Abbey Road");
-        addAlbum("Fleetwood Mac", "Abbey road");
-        expect(musicCollection).toEqual([
-            {
-                artist: "The Beatles",
-                title: "Abbey Road",
-                isPlayed: false,
-            },
-        ]);
-    });
-    it('should play album on user inputting "play $album" and set "isPlayed" to true', () => {
-        addAlbum("The Beatles", "Abbey Road");
-        playAlbum("Abbey Road");
-        expect(musicCollection).toEqual([
-            {
-                artist: "The Beatles",
-                title: "Abbey Road",
-                isPlayed: true,
-            },
-        ]);
-    });
-    it('should show all with "isPlayed" status on user inputting "show all"', () => {
-        addAlbum("The Beatles", "Abbey Road");
-        addAlbum("Fleetwood Mac", "Rumours");
-        addAlbum("Lauryn Hill", "The Miseducation of Lauryn Hill");
-        playAlbum("Abbey Road");
-        showAll();
-        expect(console.log).toHaveBeenCalledWith("The Beatles - Abbey Road");
-        expect(console.log).toHaveBeenCalledWith("Fleetwood Mac - Rumours");
-        expect(console.log).toHaveBeenCalledWith(
-            "Lauryn Hill - The Miseducation of Lauryn Hill"
-        );
-    });
+  it('should show an error on user adding invalid album to collection', () => {
+    handleAddAlbum(undefined, 'Abbey Road');
+    expect(console.log).toHaveBeenCalledWith(
+      'Invalid title and/or artist'
+    );
+  });
 
-    it('should show unplayed on user inputting "show unplayed"', () => {
-        addAlbum("The Beatles", "Abbey Road");
-        addAlbum("Fleetwood Mac", "Rumours");
-        addAlbum("Lauryn Hill", "The Miseducation of Lauryn Hill");
-        playAlbum("Abbey Road");
-        showUnplayed();
-        expect(console.log).toHaveBeenCalledWith("Fleetwood Mac - Rumours");
-        expect(console.log).toHaveBeenCalledWith(
-            "Lauren Hill - The Miseducation of Lauryn Hill"
-        );
-    });
+  it('should set "isPlayed" to true on playing album', () => {
+    handleAddAlbum('The Beatles', 'Abbey Road');
+    handlePlayAlbum('Abbey Road');
+    expect(musicCollection).toEqual([
+      {
+        artist: 'The Beatles',
+        title: 'Abbey Road',
+        isPlayed: true,
+      },
+    ]);
+  });
 
-    it('should show all by artist on user input "show all by $artist"', () => {
-        addAlbum("The Beatles", "Abbey Road");
-        addAlbum("The Beatles", "Rubber Soul");
-        addAlbum("Fleetwood Mac", "Rumours");
-        showAll("The Beatles");
-        expect(console.log).toHaveBeenCalledWith("The Beatles - Abbey Road");
-        expect(console.log).toHaveBeenCalledWith("The Beatles - Rubber Soul");
-    });
+  it('should show an error on user playing non-existent album', () => {
+    handlePlayAlbum('Abbey Road');
+    expect(console.log).toHaveBeenCalledWith(
+      '"Abbey Road" not found'
+    );
+  });
 
-    it('should show all unplayed by artist on user input "show unplayed by $artist"', () => {
-        addAlbum("The Beatles", "Abbey Road");
-        addAlbum("The Beatles", "Rubber Soul");
-        addAlbum("Fleetwood Mac", "Rumours");
-        playAlbum("Abbey Road");
-        showUnplayed("The Beatles");
-        expect(console.log).toHaveBeenCalledWith("The Beatles - Rubber Soul");
-    });
-    // TODO maybe it('should maybe: should be case insensitive when checking for duplicates and on user inputting "quit"', () => {});
+  it('should show all with "isPlayed" status on "show all"', () => {
+    musicCollection.push(...testCollection);
+    handleShowAlbums();
+    expect(console.log).toHaveBeenCalledWith(
+      '"Abbey Road" by The Beatles (played)'
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      '"Rumours" by Fleetwood Mac (unplayed)'
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      '"The Miseducation of Lauryn Hill" by Lauryn Hill (unplayed)'
+    );
+  });
 
-    // TODO maybe it('maybe: should escape quotes/sanitize text', () => {});
-    // TODO maybe also: colocate all user input tests in separate describe block?
-    // it('maybe: init', () => {
-    // TODO maybe    // text input for users, empty library, 'welcome to your music collection'
-    // });
+  it('should show unplayed on "show unplayed"', () => {
+    musicCollection.push(...testCollection);
+    handleShowUnplayed();
+    expect(console.log).toHaveBeenCalledWith(
+      '"Rumours" by Fleetwood Mac'
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      '"The Miseducation of Lauryn Hill" by Lauryn Hill'
+    );
+  });
 
-    // getUserInput('show all');
-    // 'youre listening to $album'
+  it('should show all by artist "show all by $artist"', () => {
+    musicCollection.push(...testCollection);
+    handleShowAlbums('The Beatles');
+    expect(console.log).toHaveBeenCalledWith(
+      '"Abbey Road" by The Beatles (played)'
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      '"Rubber Soul" by The Beatles (unplayed)'
+    );
+  });
+
+  it('should show unplayed by artist on "show unplayed by $artist"', () => {
+    musicCollection.push(...testCollection);
+    handleShowUnplayed('The Beatles');
+    expect(console.log).toHaveBeenCalledWith(
+      '"Rubber Soul" by The Beatles'
+    );
+  });
 });
